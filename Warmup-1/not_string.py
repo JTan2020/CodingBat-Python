feb966d114948b582bc7102769473b3cdde1ba51
JTan2020 <64496112+JTan2020@users.noreply.github.com>
2020-05-31 21:58:34 -0700

    Create not_string.py

---
+++ Warmup-1/not_string.py
@@ -0,0 +1,13 @@
+#Given a string, return a new string where "not " has been added to the front.
+#However, if the string already begins with "not", return the string unchanged.
+
+
+#not_string('candy') → 'not candy'
+#not_string('x') → 'not x'
+#not_string('not bad') → 'not bad'
+
+#code
+def not_string(str):
+  if 'not' != str[0:3]:
+    str = 'not '+str
+  return(str)
